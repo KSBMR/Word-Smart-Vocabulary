d20a348fccd1a73ceb76a8b8d8723cdaf87efcb2
Tasara-Limited <tasarait@gmail.com>
2026-07-25 23:44:12 +0600

    Add STATIC_ROOT and update build script

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,9 @@
-﻿#!/bin/bash
+#!/bin/bash
 set -e
 echo "Installing dependencies..."
 pip install -r requirements.txt
+echo "Creating static directory..."
+mkdir -p staticfiles
 echo "Running migrations..."
 python manage.py migrate
 echo "Collecting static files..."
