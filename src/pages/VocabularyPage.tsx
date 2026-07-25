e28eaefc426b328d534a17abaa973373a32661ef
Tasara-Limited <tasarait@gmail.com>
2026-07-25 14:53:54 +0600

    Hi

--- src/pages/VocabularyPage.tsx
+++ src/pages/VocabularyPage.tsx
@@ -264,7 +264,7 @@ export default function VocabularyPage() {
           }
         }}
         onWordSelect={openWordDetails}
-        {/* onLetterClick={() => setLetterPickerOpen(true)} */}
+       
       />
 
       {/* Letter Picker Modal */}
