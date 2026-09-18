9dee6aa9e9fe05e24276d6b5aa14764b010df606
Tasara-Limited <tasarait@gmail.com>
2026-09-18 20:29:27 +0600

    again

--- frontend/src/components/layout/AppLayout.tsx
+++ frontend/src/components/layout/AppLayout.tsx
@@ -4,24 +4,23 @@ import MobileNav from './MobileNav';
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen bg-background">
-      <div className="flex">
-        {/* Desktop Sidebar */}
-        <aside className="hidden md:block w-64 shrink-0 border-r border-border/60">
-          <div className="sticky top-0 h-screen">
-            <Sidebar />
-          </div>
-        </aside>
+    <>
+      {/* Desktop Sidebar - fixed position, out of normal flow */}
+      <aside className="hidden md:fixed md:left-0 md:top-0 md:bottom-0 md:w-64 md:border-r md:border-border/60 md:bg-background md:z-40">
+        <Sidebar />
+      </aside>
 
-        {/* Main Content */}
-        <main className="flex-1 min-w-0 p-4 md:p-6 lg:p-8 pb-24 md:pb-8">
+      {/* Main content - pushed right by sidebar on desktop */}
+      <div className="md:ml-64 min-h-screen">
+        <main className="p-4 md:p-6 lg:p-8 pb-24 md:pb-8">
           <div className="max-w-7xl mx-auto">
             <Outlet />
           </div>
         </main>
       </div>
 
+      {/* Mobile bottom nav */}
       <MobileNav />
-    </div>
+    </>
   );
 }
